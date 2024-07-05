a6cfae3fdb67981e145138a525b26df0278f546c
Roy <rojusbalciunas@gmail.com>
2024-07-05 20:48:47 +0300

    added: link to show by brand card

--- components/layout/ShopUiElements/ShopbyBrand/ShopByBrand.tsx
+++ components/layout/ShopUiElements/ShopbyBrand/ShopByBrand.tsx
@@ -3,6 +3,7 @@ import { cookies } from "next/headers";
 import React from "react";
 import ShopByBrandItem from "./ShopByBrandItem";
 import Image from "next/image";
+import Link from "next/link";
 
 export const revalidate = 360;
 
@@ -13,21 +14,24 @@ const ShopByBrand = async () => {
     .from("brands")
     .select()
     .neq("brand_image_url", null);
+
   return (
     <section>
       <span className="text-2xl font-semibold">Shop by Brand</span>
-      <ul className="grid grid-cols-5 gap-8 mt-8">
+      <ul className="mt-8 grid grid-cols-5 gap-8">
         {shopByBrand?.map((el) => (
           <li key={el.id} className="text-center">
-            <ShopByBrandItem>
-              <Image
-                alt={el.brand_name + " logo"}
-                height={120}
-                width={120}
-                src={el.brand_image_url || ""}
-              />
-            </ShopByBrandItem>
-            <span>{el.brand_name}</span>
+            <Link href={`/categories/${el.brand_name}`}>
+              <ShopByBrandItem>
+                <Image
+                  alt={el.brand_name + " logo"}
+                  height={120}
+                  width={120}
+                  src={el.brand_image_url || ""}
+                />
+              </ShopByBrandItem>
+              <span>{el.brand_name}</span>
+            </Link>
           </li>
         ))}
       </ul>
